ac0d1322f81e52cf01db144325700be571b3eb69
Rajesh1099 <rajesh.lingampalli10@gmail.com>
2024-03-21 14:21:02 +0530

    first commit

---
+++ server/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "OG3XUKtkEEC+9bZ5ZQq+XtGAFU/18BxhfdD9VDPih+o=",
+    EMAIL: "fleta.morar@ethereal.email", // testing email & password
+    PASSWORD : "Ma4v4T8bSTqPhBEEcw",
+    ATLAS_URI: "mongodb+srv://tonystarc1099:tonystarc1099@cluster0.8anwuqn.mongodb.net/"
+}
